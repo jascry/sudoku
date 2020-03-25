b2e3d702b336caa024e5a104785f2ad442d05672
Daniel Kopleński <dkoplenski@gmail.com>
2020-03-25 18:58:28 +0100

    fix: solved sudoku with backtracking algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  solve(matrix);
+  return matrix;
+}
+
+function checkIfNotRepeated(board, row, column, value) {
+  for(let i = 0; i < 9; i++) {
+    const a = 3 * Math.floor(row/3) + Math.floor(i/3);
+    const b = 3 * Math.floor(column/3) + i % 3;
+    if(board[row][i] == value || board[i][column] == value || board[a][b] == value) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function solve(matrix) {
+  for(let i = 0; i < 9; i++) {
+    for(let j = 0; j < 9; j++) {
+      if(matrix[i][j] == 0) {
+        for(let k = 1; k <=9; k++) {
+          if(checkIfNotRepeated(matrix, i, j, k)) {
+            matrix[i][j] = k;
+            if(solve(matrix)) {
+              return true;
+            } else {
+              matrix[i][j] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
 }
